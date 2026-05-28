88b39b8610da0ab293c9f5706114af492f74943b
amal <amalkrdani0668@gmail.com>
2026-05-28 17:53:54 +0100

    Ajout des modèles dbt

---
+++ dbt_pipeline/models/ventes_clean.sql
@@ -0,0 +1,12 @@
+select
+    date,
+    produit,
+    categorie,
+    quantite,
+    prix_unitaire,
+    ville,
+    (quantite * prix_unitaire) as chiffre_affaires
+from ventes_raw
+where
+    quantite > 0
+    and prix_unitaire > 0
